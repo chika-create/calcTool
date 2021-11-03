6d19d96bf1a8d742939141cc42059639de7ba55e
chika <rejuvenator@hotmail.co.jp>
2021-11-03 17:56:18 +0900

    toggle Button追加

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
@@ -6,7 +7,8 @@ import Box from "@mui/material/Box";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -48,6 +50,15 @@ export default function Home() {
     setValue(newValue);
   };
 
+  const [alignment, setAlignment] = React.useState<string | null>("left");
+
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setAlignment(newAlignment);
+  };
+
   return (
     <div>
       <Box sx={{ width: "100%" }}>
@@ -58,10 +69,41 @@ export default function Home() {
             aria-label="basic tabs example"
           >
             <Tab label="傾国" {...a11yProps(0)} />
-            <Tab label="群雄" {...a11yProps(1)} />
+            <Tab label="群雄" {...a11yProps(1)} disabled />
             <Tab label="おにぎり表" {...a11yProps(2)} />
           </Tabs>
         </Box>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>初期設定</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>赤城 騎馬数（デッキ人数）</Typography>
+            <ToggleButtonGroup
+              value={alignment}
+              exclusive
+              onChange={handleAlignment}
+              aria-label="text alignment"
+            >
+              <ToggleButton value="left" aria-label="left aligned">
+                1
+              </ToggleButton>
+              <ToggleButton value="center" aria-label="centered">
+                2
+              </ToggleButton>
+              <ToggleButton value="right" aria-label="right aligned">
+                3
+              </ToggleButton>
+              <ToggleButton value="justify" aria-label="justified">
+                4
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </AccordionDetails>
+        </Accordion>
         <TabPanel value={value} index={0}>
           傾国のおにぎり計算エリア
         </TabPanel>
@@ -72,20 +114,6 @@ export default function Home() {
           おにぎり一覧表を表示
         </TabPanel>
       </Box>
-      <div>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>初期設定</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>赤城 青城 金城</Typography>
-          </AccordionDetails>
-        </Accordion>
-      </div>
     </div>
   );
 }
